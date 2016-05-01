132f01b3ecd2aeb7eb00bf6c2801769eeceed26b
muhdamrullah <muneorion@gmail.com>
2016-05-01 10:37:53 +0800

    Added API function

--- index.js
+++ index.js
@@ -15,10 +15,19 @@ app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('index');
 });
+
+app.get('/api/', function(req, res){
+  res.render('index');
+});
+
+var baseURL = '<insert your server here>/uploads';
+var specialURL = '<insert special server here>';
 
 // index.js continued
 app.post('/', multer({ dest: './uploads/'}).single('imgFile'), function(req,res){
@@ -28,9 +37,8 @@ app.post('/', multer({ dest: './uploads/'}).single('imgFile'), function(req,res)
         console.log(req.file.path);
 
 	// This prepares to send the URL to the external server
-	var baseURL = '<insert local server here>/uploads'
         var baseimageURL = baseURL + req.file.filename;
-	var MIT = request('<insert special server here>'+baseimageURL, function (error, response, data) {
+	var MIT = request(specialURL + baseimageURL, function (error, response, data) {
 	    if (!error && response.statusCode == 200) {
 	    var FacerecogData = JSON.parse(data)
 
@@ -49,7 +57,7 @@ app.post('/', multer({ dest: './uploads/'}).single('imgFile'), function(req,res)
 	    };
 
 	    // This sends the data in the jsend-compatible format
-	    var jsendData = jsend.success({ recall_score: MITData.memscore, base_image_url: baseimageURL, heatmap_url : postimageURL}); 
+	    var jsendData = jsend.success({ recall_score: FacerecogData.memscore, base_image_url: baseimageURL, heatmap_url : postimageURL}); 
 
 	    // Check file exist
 	    specialrequest.on('close', function () {
@@ -61,13 +69,41 @@ app.post('/', multer({ dest: './uploads/'}).single('imgFile'), function(req,res)
 	    setInterval(function() {
 		res.end();
 	    },2000);
-
-//            res.json(jsendData).end();
             });
 });
 
-app.use('/uploads', express.static(__dirname + '/uploads'));
-app.use('/public', express.static(__dirname + '/public'));
+app.post('/api/', multer({ dest: './uploads/'}).single('imgFile'), function(req,res){
+	// This is the form fields
+        console.log(req.body);
+        console.log(req.file);
+        console.log(req.file.path);
+
+	// This prepares to send the URL to the external server
+        var apibaseimageURL = baseURL + req.file.filename;
+	var apiFacerecog = request(specialURL + apibaseimageURL, function (error, response, data) {
+	    if (!error && response.statusCode == 200) {
+	    var apiFacerecogData = JSON.parse(data)
+
+	    // This is a test to check that data received is correct
+            console.log(apiFacerecogData);
+	    console.log(apiFacerecogData.heatmap);
+
+	    // This uses the fs and request library to download image off the external server
+	    var apispecialrequest = request(apiFacerecogData.heatmap)
+	    apispecialrequest.on('response',  function (res) {
+  	  	res.pipe(fs.createWriteStream('../uploads/' + req.file.filename + '.' + res.headers['content-type'].split('/')[1]));
+		});	    
+
+	    // This prepares the post image processing URL
+	    var apipostimageURL = baseURL + req.file.filename + '.jpeg' 
+	    };
+
+	    // This sends the data in the jsend-compatible format
+	    var apijsendData = jsend.success({ recall_score: apiFacerecogData.memscore, base_image_url: apibaseimageURL, heatmap_url : apipostimageURL}); 
+
+            res.json(jsendData).end();
+            });
+});
 
 var port = 5000;
 app.listen( port, function(){ console.log('listening on port '+port); } );
