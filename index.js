18b76c008a6ddd7687d01c620ef195078b96e0fc
muhdamrullah <muneorion@gmail.com>
2016-05-01 10:14:17 +0800

    Changed port 5000

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // index.js start of file
-var express = require('express')
-    , cors = require('cors');
-var multer = require('multer'),
-        bodyParser = require('body-parser'),
-        path = require('path');
-var oxfordEmotion = require("node-oxford-emotion")('<insert here>');
-var fs = require('fs');
-var jsend = require('jsend');
-var request = require('request');
+var express = require('express'),
+    cors = require('cors')
+    multer = require('multer'),
+    bodyParser = require('body-parser'),
+    path = require('path');
+    fs = require('fs');
+    jsend = require('jsend');
+    request = require('request');
 
 var app = new express();
 app.use(bodyParser.json());
@@ -29,32 +28,46 @@ app.post('/', multer({ dest: './uploads/'}).single('imgFile'), function(req,res)
         console.log(req.file.path);
 
 	// This prepares to send the URL to the external server
-        var baseimageURL = '<insert server url here>/' + req.file.filename;
-	var MIT = request('<insert special url here>' + baseimageURL, function (error, response, data) {
+	var baseURL = '<insert local server here>/uploads'
+        var baseimageURL = baseURL + req.file.filename;
+	var MIT = request('<insert special server here>'+baseimageURL, function (error, response, data) {
 	    if (!error && response.statusCode == 200) {
-	    var MITData = JSON.parse(data)
+	    var FacerecogData = JSON.parse(data)
 
 	    // This is a test to check that data received is correct
-            console.log(MITData);
-	    console.log(MITData.heatmap);
+            console.log(FacerecogData);
+	    console.log(FacerecogData.heatmap);
 
 	    // This uses the fs and request library to download image off the external server
-	    var specialrequest = request(MITData.heatmap)
+	    var specialrequest = request(FacerecogData.heatmap)
 	    specialrequest.on('response',  function (res) {
   	  	res.pipe(fs.createWriteStream('./uploads/' + req.file.filename + '.' + res.headers['content-type'].split('/')[1]));
-		});
+		});	    
 
 	    // This prepares the post image processing URL
-	    var postimageURL = '<insert server url here>/' + req.file.filename + '.jpeg' 
+	    var postimageURL = baseURL + req.file.filename + '.jpeg' 
 	    };
 
 	    // This sends the data in the jsend-compatible format
 	    var jsendData = jsend.success({ recall_score: MITData.memscore, base_image_url: baseimageURL, heatmap_url : postimageURL}); 
-            res.json(jsendData).end();
+
+	    // Check file exist
+	    specialrequest.on('close', function () {
+		console.log('Finished Download');
+	    });
+
+	    // Sets 2 second delay
+	    res.write('<html><head><title>Facerecog Asia</title><link href="./public/index.css" rel="stylesheet"></head><body><div class="wrapper"><div class="inner"><img class="img" src="./'+ req.file.path +'" /></div><div class="inner"><img class="img" src="./'+ req.file.path +'.jpeg" style="opacity:0.5" /></div><h2>Predicted Recall Rate: '+jsendData.data.recall_score+'</h2></div></body></html>');
+	    setInterval(function() {
+		res.end();
+	    },2000);
+
+//            res.json(jsendData).end();
             });
 });
 
-app.use(express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/public', express.static(__dirname + '/public'));
 
-var port = 3000;
+var port = 5000;
 app.listen( port, function(){ console.log('listening on port '+port); } );
